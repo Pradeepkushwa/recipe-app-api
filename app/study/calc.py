7cd96699d1050915a5afb9c5fe5f1ce79f9592a8
Pradeep <pradeepkushwaha232365@gmail.com>
2025-05-13 14:09:00 +0530

    Added test cases

---
+++ app/study/calc.py
@@ -0,0 +1,13 @@
+"""
+  calculate funciton
+"""
+
+def add(x,y):
+    """
+    Addition two values 
+    """
+    return x + y
+
+def subtract(x,y):
+    """ substraction two values"""
+    return y - x
